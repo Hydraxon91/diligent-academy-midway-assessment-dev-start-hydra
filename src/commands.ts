ce34daa7fa632af960964b7cad787538ff0689b4
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-10-21 16:28:05 +0200

    implements rest of details

--- src/commands.ts
+++ src/commands.ts
@@ -20,11 +20,15 @@ export async function details(store: Store<RecipeType[]>, args: string[]){
   if (args.length!==1) {
     console.error(`Details shouldn't have 1 argument which is a number`)
   }
-  if (isNaN(Number(args[0]))) {
+  const arg = Number(args[0])
+  if (isNaN(arg)) {
     console.error(`'${args[0]}' is not a number.`);
   }
   else{
-    
-    
+    const recipe = new Recipe(store);
+    const recipes = await recipe.readAll();
+    const requestedRecipe = recipes.find(recipe => recipe.id === arg);
+    console.log(`ID: ${requestedRecipe?.id}`);
+    console.log(`Name:: ${requestedRecipe?.name}`);
   }
 }
